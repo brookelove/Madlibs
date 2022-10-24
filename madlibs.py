89a6d890b43a559cbe5e19024538714f2d7267ee
brookelove <brookelove@Brookes-iMac-2.local>
2022-10-24 15:06:11 -0700

    creating my own madlib

--- madlibs.py
+++ madlibs.py
@@ -14,3 +14,10 @@ famous_person = input("Famous person: ")
 
 madlib = f"Computer program is so {adj}! it makes me so excited all the time because \ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 print(madlib)
+
+verb3 = input("verb 3: ")
+verb4 = input("adjective ")
+location = input("location: ")
+madlib_2 = f"I love to {verb3} because it makes me feel free. I think that tacos are {adj2}. I want to go to {location} because it is my next adventure."
+
+print(madlib_2)
